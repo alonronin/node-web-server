c9bc6ffdf9b4cb085fcb6a6fd16e6a33c3a386ee
Alon Valadji <alon@empeeric.com>
2012-07-15 05:39:34 +0300

    taking out the cb to allocate function

--- app.js
+++ app.js
@@ -12,34 +12,36 @@ sites.push({'address' : 'test2.roninil.com', 'path' : 'c://www/test2/app.js'});
 sites.push({'address' : 'webic.roninil.com', 'path' : 'c://www/Webic-CMAss/app.js'});
 sites.push({'address' : 'chat.roninil.com', 'path' : 'c://www/chat/app.js'});
 
+var allocate = function(err, port){
+    var env = {};
+    env.PORT = port;
+
+    var child = spawn('node', [sites[i].path], {env: env});
+
+    child.on('exit', function(code){
+        console.error('http://%s:%d -- stopped with code %d', host, port, code);
+    });
+
+    console.log('http://%s:%d -- starting', host, port);
+
+    setTimeout(function(){
+        port_scanner.checkPortStatus(port, host, function(err, status){
+            if(status == 'open') {
+                sites[i].port = port;
+                console.log('http://%s:%d -- started', host, port);
+                i++;
+                launch();
+            }else{
+                console.error("can't start server");
+                console.log(arguments);
+            }
+        })
+    }, 800);
+};
+
 var launch = function(){
     if(i < sites.length){
-        port_scanner.findAPortNotInUse(port_range[0], port_range[1], host, function(err, port){
-            var env = {};
-            env.PORT = port;
-
-            var child = spawn('node', [sites[i].path], {env: env});
-
-            child.on('exit', function(code){
-                console.error('http://%s:%d -- stopped with code %d', host, port, code);
-            });
-
-            console.log('http://%s:%d -- starting', host, port);
-
-            setTimeout(function(){
-                port_scanner.checkPortStatus(port, host, function(err, status){
-                    if(status == 'open') {
-                        sites[i].port = port;
-                        console.log('http://%s:%d -- started', host, port);
-                        i++;
-                        launch();
-                    }else{
-                        console.error("can't start server");
-                        console.log(arguments);
-                    }
-                })
-            }, 800);
-        });
+        port_scanner.findAPortNotInUse(port_range[0], port_range[1], host, allocate);
     }else{
         var options = {
             router: {}
